36452e9a0154c025c21403362bbcb486ddc36eb2
talvezToto <joheflopez1@gmail.com>
2025-07-28 15:21:38 -0500

    szs

---
+++ SRC/App.js
@@ -0,0 +1,15 @@
+import Saludo from './Saludo';
+
+function App() {
+  return (
+    <div>
+      <h1>¡Hola React!</h1>
+      <p>Este es mi primer componente.</p>
+      <Saludo nombre = "Santiago" /> 
+      <Saludo nombre = "Laura" /> 
+    </div>
+  );
+}
+
+export default App; 
+
---
+++ SRC/Saludo.js
@@ -0,0 +1,5 @@
+function Saludo(props) {
+  return <h2>Hola {props.nombre}, ¡bienvenido a React!</h2>;
+}
+export default Saludo;
+
